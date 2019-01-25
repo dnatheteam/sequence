f7f559f644b548ac5e1fcb22b00839dfae03ca3b
Edem Abduraimov <uncle.jorj@gmail.com>
2019-01-25 22:08:28 +0300

    full optimization

--- src/Sequency.js
+++ src/Sequency.js
@@ -1,80 +1,32 @@
-import React, { Component } from "react";
-import styled from "styled-components";
+import styled, { keyframes } from 'styled-components';
 
-const Sequence = styled.div`
-  width: ${props => props.width + "px"};
-  height: ${props => props.height + "px"};
-  overflow: hidden;
+const ANIMATION = keyframes`
+  100% {
+    background-position: 100% 0;
+  }
 `;
 
-class Sequency extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      animationDuration: this.props.animationDuration,
-      cycle: this.props.cycle,
-      delay: this.props.delay,
-      frameCount: this.props.frameCount,
-      frameWidth: this.props.frameWidth,
-      frameHeight: this.props.frameHeight,
-      transform: "translate(0px,0)"
-    };
+export let Sequency = styled.div`
+  position: relative;
+  display: block;
+  top: 0;
+  left: 0;
+  width: ${ props => props.width || '100%'};
+  height: ${ props => props.height || '100%' };
+  overflow: hidden;
+  will-change: background-position;
 
-    this.changeFrames = this.changeFrames.bind(this);
-  }
+  background-position: 0 0;
+  background-repeat: no-repeat;
+  background-size: cover;
+  background-color: transparent;
+  background-image: url(${props => props.src});
 
-  changeFrames() {
-    const repeatCount = Math.round(
-      this.state.animationDuration / this.state.frameCount
-    );
-
-    let sequencePosition = 0,
-      count = 0,
-      endAnimationTimeout;
-
-    const endAnimation = () => {
-      if (this.state.cycle) {
-        sequencePosition = 0;
-        count = 0;
-      } else {
-        clearInterval(endAnimationTimeout);
-      }
-    };
-
-    const startAnimation = () => {
-      sequencePosition -= this.state.frameWidth;
-      count++;
-      this.setState({
-        transform: "translate(" + sequencePosition + "px,0)"
-      });
-      endAnimationTimeout = setTimeout(startAnimation, repeatCount);
-      if (count === this.state.frameCount) {
-        endAnimation();
-      }
-    };
-
-    setTimeout(() => {
-      setTimeout(startAnimation, repeatCount);
-    }, this.state.delay);
-  }
-
-  componentDidMount() {
-    this.changeFrames();
-  }
-
-  render() {
-    const imgStyles = {
-      transform: this.state.transform
-    };
-
-    return (
-      <>
-        <Sequence width={this.state.frameWidth} height={this.state.frameHeight}>
-          <img src={this.props.imageSrc} alt="sequence" style={imgStyles} />
-        </Sequence>
-      </>
-    );
-  }
-}
-
-export default Sequency;
+  animation-name: ${ ANIMATION };
+  animation-duration: ${ props => props.duration || 1 }ms;
+  animation-delay: ${ props => props.delay || 0 }ms;
+  animation-iteration-count: ${ props => props.iterations || 'infinite' };
+  animation-timing-function: steps(${ props => props.frames || 0 });
+  animation-fill-mode: ${ props => props.mode || 'forwards' };
+  animation-direction: ${ props => props.direction || 'normal' };
+`;
--- src/index.js
+++ src/index.js
@@ -1,44 +1,42 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
-import Sequency from "./Sequency";
-import * as serviceWorker from "./serviceWorker";
+import { Sequency } from "./Sequency";
+
 import example_1 from "./sprite-1.png";
 import example_2 from "./sprite-2.png";
 import example_3 from "./sprite-3.png";
 
 ReactDOM.render(
-  <>
+  <div>
     <Sequency
-      imageSrc={example_1}
-      animationDuration={1000}
-      delay={1000}
-      frameCount={29}
-      frameWidth={120}
-      frameHeight={120}
+      src = { example_1 }
+      duration = { 1000 }
+      delay = { 1000 }
+      frames = { 29 }
+      width = '120px'
+      height = '120px'
+      mode = 'backwards'
+      direction = 'alternate-reverse'
     />
     <Sequency
-      imageSrc={example_2}
-      animationDuration={2000}
-      delay={3000}
-      frameCount={147}
-      frameWidth={306}
-      frameHeight={306}
+      src = { example_2 }
+      duration = { 2000 }
+      delay = { 3000 }
+      frames = { 147 }
+      width = '306px'
+      height = '306px'
+      iterations = { 1 }
+    />
     />
     <Sequency
-      imageSrc={example_3}
-      animationDuration={5000}
-      delay={0}
-      frameCount={122}
-      frameWidth={208}
-      frameHeight={200}
-      cycle
+      src = { example_3 }
+      duration = { 5000 }
+      delay = { 0 }
+      frames = { 122 }
+      width = '208px'
+      height = '200px'
     />
-  </>,
+  </div>,
   document.getElementById("root")
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
