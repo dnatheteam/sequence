17fa2d40f7bac36ad353f3de15d50fd1abaa610e
mmmaxeo <walkworksleep@gmail.com>
2019-04-18 17:25:13 +0300

    Prettier and correct frames prop

--- src/Sequency.js
+++ src/Sequency.js
@@ -1,4 +1,4 @@
-import styled, { keyframes } from 'styled-components';
+import styled, { keyframes } from "styled-components";
 
 export const ANIMATION = keyframes`
   100% {
@@ -11,8 +11,8 @@ export const Sequency = styled.div`
   display: block;
   top: 0;
   left: 0;
-  width: ${ props => props.width || '100%'};
-  height: ${ props => props.height || '100%' };
+  width: ${props => props.width || "100%"};
+  height: ${props => props.height || "100%"};
   overflow: hidden;
   will-change: background-position;
 
@@ -22,12 +22,11 @@ export const Sequency = styled.div`
   background-color: transparent;
   background-image: url(${props => props.src});
 
-  animation-name: ${ ANIMATION };
-  animation-duration: ${ props => props.duration || 1000 }ms;
-  animation-delay: ${ props => props.delay || 0 }ms;
-  animation-iteration-count: ${ props => props.iterations || 'infinite' };
-  animation-timing-function: steps(${ props => props.frames || 0 });
-  animation-fill-mode: ${ props => props.mode || 'forwards' };
-  animation-direction: ${ props => props.direction || 'normal' };
-
+  animation-name: ${ANIMATION};
+  animation-duration: ${props => props.duration || 1000}ms;
+  animation-delay: ${props => props.delay || 0}ms;
+  animation-iteration-count: ${props => props.iterations || "infinite"};
+  animation-timing-function: steps(${props => props.frames - 1 || 0});
+  animation-fill-mode: ${props => props.mode || "forwards"};
+  animation-direction: ${props => props.direction || "normal"};
 `;
--- src/index.js
+++ src/index.js
@@ -1,41 +1,41 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import { Sequency } from './Sequency';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import { Sequency } from "./Sequency";
 
-import firstSprite from './sprite-1.png';
-import secondSprite from './sprite-2.png';
-import thirdSprite from './sprite-3.png';
+import firstSprite from "./sprite-1.png";
+import secondSprite from "./sprite-2.png";
+import thirdSprite from "./sprite-3.png";
 
 ReactDOM.render(
   <div>
     <Sequency
-      src = { firstSprite }
-      duration = { 1000 }
-      delay = { 1000 }
-      frames = { 29 }
-      width = "120px"
-      height = "120px"
-      mode = "backwards"
-      direction = "alternate-reverse"
+      src={firstSprite}
+      duration={1000}
+      delay={1000}
+      frames={30}
+      width="120px"
+      height="120px"
+      mode="backwards"
+      direction="alternate-reverse"
     />
     <Sequency
-      src = { secondSprite }
-      duration = { 2000 }
-      delay = { 3000 }
-      frames = { 147 }
-      width = "306px"
-      height = "306px"
-      iterations = { 1 }
+      src={secondSprite}
+      duration={2000}
+      delay={3000}
+      frames={148}
+      width="306px"
+      height="306px"
+      iterations={1}
     />
     <Sequency
-      src = { thirdSprite }
-      duration = { 5000 }
-      delay = { 0 }
-      frames = { 122 }
-      width = "208px"
-      height = "200px"
+      src={thirdSprite}
+      duration={5000}
+      delay={0}
+      frames={123}
+      width="208px"
+      height="200px"
     />
   </div>,
-  document.getElementById('root') // eslint-disable-line
+  document.getElementById("root") // eslint-disable-line
 );
