7a2da93eeb28271454ba3699215ab50f1de6a00e
mmmaxeo <walkworksleep@gmail.com>
2019-05-14 19:27:01 +0300

    Data from ImageUpload component to App

--- src/ImageUpload.js
+++ src/ImageUpload.js
@@ -1,53 +1,26 @@
 import React, { Component } from "react";
-import Editor from "./Editor";
 
 class ImageUpload extends Component {
-  constructor(props) {
-    super(props);
-    this.state = { file: "", imageUrl: "" };
-  }
-
-  handleSubmit(e) {
-    e.preventDefault();
-    // TODO: do something with -> this.state.file
-    console.log("handle uploading-", this.state.file);
-  }
-
-  handleImageChange(e) {
-    e.preventDefault();
-
-    let reader = new FileReader();
-    let file = e.target.files[0];
-    reader.onloadend = () => {
-      this.setState({
-        file: file,
-        imagePreviewUrl: reader.result
-      });
-    };
-
-    reader.readAsDataURL(file);
-  }
   render() {
-    let { imagePreviewUrl } = this.state;
+    let { handleSubmit, handleImageChange } = this.props;
 
     return (
-      <div className="previewComponent">
-        <form onSubmit={e => this.handleSubmit(e)}>
+      <>
+        <form onSubmit={e => handleSubmit(e)}>
           <input
             className="fileInput"
             type="file"
-            onChange={e => this.handleImageChange(e)}
+            onChange={e => handleImageChange(e)}
           />
           <button
             className="submitButton"
             type="submit"
-            onClick={e => this.handleSubmit(e)}
+            onClick={e => handleSubmit(e)}
           >
             Upload Image
           </button>
         </form>
-        <Editor imageUrl={imagePreviewUrl} />
-      </div>
+      </>
     );
   }
 }
